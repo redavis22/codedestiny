9546ba06862dd1308312de9f203f69c9aca2cb51
redavis22 <redavis22@gmail.com>
2016-08-05 15:41:36 -0400

    response to comment

--- player_health.py
+++ player_health.py
@@ -4,6 +4,11 @@ player_health=200
 
 stab = -1
 #poisonus damagae decreases health for 5, max of 20
+
+#ok this while need something called a "for loop"
+#once hit with poisonus stab, decresase player health by 5 points until 20 points are decreased
+#I'll go back to see how to code it
+
 injury = player_health + 20*stab
 
 print injury
